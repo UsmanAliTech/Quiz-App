051a511a701ef9879d301959ceaacc490dc18dc0
UsmanAliTech <usmandev502@gmail.com>
2025-12-18 20:53:18 -0800

    working in home page

--- src/Conponents/Home.jsx
+++ src/Conponents/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Home = () => {
   return (
     <div>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Et neque placeat optio cumque minus nisi cum necessitatibus sed at delectus dolore vitae, ea enim ab nulla veniam ipsa similique. Reiciendis.
+        
       
     </div>
   )
