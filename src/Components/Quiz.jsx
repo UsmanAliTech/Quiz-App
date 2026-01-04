34ee7f7bbeb1f14e226d368127d9a5e6ad4eca23
UsmanAliTech <usmandev502@gmail.com>
2026-01-04 07:47:17 -0800

    ...

--- src/Components/Quiz.jsx
+++ src/Components/Quiz.jsx
@@ -1,27 +1,12 @@
 import React, { useState } from 'react'
-import quizdata from '../Data/Quizdata';
-const Quiz = () => {
-    const [CurrrentQuestion,SetCurrentQuestion]=useState(0);
-    const [SelectedAnswer,SetSelectedAnswer]=useState('');
-    const [Score,SetScore]=useState(0);
-    const [Result,ShowResult]=useState(false);
 
-    const HandleNext=()=>{
-        if(
-            SelectedAnswer===question[CurrrentQuestion].answer
-        ){
-            SetScore(Score + 1);
-        }
-        
-    }
+const Quiz = () => {
+  const [CurrentIndex, setCurrentIndex]=useState(0)
+const [Score, setScore]=useState(0)
   return (
+
     <div>
-      <h2>{question[CurrrentQuestion].question}</h2>
-      {question[CurrrentQuestion].option s.map((option,index)=>{
-        <button key={index}
-        onClick={()=>SetSelectedAnswer(option)}
-        >{option}</button>
-      })}
+      
     </div>
   )
 }
